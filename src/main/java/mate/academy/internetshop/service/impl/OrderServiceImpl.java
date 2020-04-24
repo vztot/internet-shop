58fc7e9ae1c498e82ee761c06d74aa0530af84db
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-04-24 10:50:15 +0300

    added "clearBucket" in completeOrder();

--- src/main/java/mate/academy/internetshop/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/internetshop/service/impl/OrderServiceImpl.java
@@ -1,12 +1,15 @@
 package mate.academy.internetshop.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import mate.academy.internetshop.dao.OrderDao;
+import mate.academy.internetshop.dao.ShoppingCartDao;
 import mate.academy.internetshop.lib.Inject;
 import mate.academy.internetshop.lib.Service;
 import mate.academy.internetshop.model.Order;
 import mate.academy.internetshop.model.Product;
+import mate.academy.internetshop.model.ShoppingCart;
 import mate.academy.internetshop.model.User;
 import mate.academy.internetshop.service.OrderService;
 
@@ -16,8 +19,15 @@ public class OrderServiceImpl implements OrderService {
     @Inject
     private OrderDao orderDao;
 
+    @Inject
+    private ShoppingCartDao shoppingCartDao;
+
     @Override
     public Order completeOrder(List<Product> products, User user) {
+        ShoppingCart shoppingCartOfUser = shoppingCartDao.getAll().stream().
+                filter(shoppingCart -> shoppingCart.getUser().equals(user)).findFirst().get();
+        shoppingCartOfUser.setProducts(new ArrayList<>());
+        shoppingCartDao.update(shoppingCartOfUser);
         return orderDao.create(new Order(products, user));
     }
 
