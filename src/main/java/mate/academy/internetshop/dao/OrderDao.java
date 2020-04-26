02bfe9ba46bf6a6910b47c4f8a53045d6d5ddb9b
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-04-26 15:58:48 +0300

    Added GenericDao and GenericService

--- src/main/java/mate/academy/internetshop/dao/OrderDao.java
+++ src/main/java/mate/academy/internetshop/dao/OrderDao.java
@@ -1,18 +1,6 @@
 package mate.academy.internetshop.dao;
 
-import java.util.List;
-import java.util.Optional;
 import mate.academy.internetshop.model.Order;
 
-public interface OrderDao {
-
-    Order create(Order order);
-
-    Optional<Order> get(Long id);
-
-    List<Order> getAll();
-
-    Order update(Order order);
-
-    boolean delete(Long id);
+public interface OrderDao extends GenericDao<Order, Long> {
 }
--- src/main/java/mate/academy/internetshop/service/ProductService.java
+++ src/main/java/mate/academy/internetshop/service/ProductService.java
@@ -1,19 +1,7 @@
 package mate.academy.internetshop.service;
 
-import java.util.List;
 import mate.academy.internetshop.model.Product;
 
-public interface ProductService {
-
-    Product create(Product product);
-
-    Product get(Long id);
-
-    List<Product> getAll();
-
-    Product update(Product product);
-
-    boolean delete(Long id);
-
+public interface ProductService extends GenericService<Product, Long> {
     boolean deleteByProduct(Product product);
 }
