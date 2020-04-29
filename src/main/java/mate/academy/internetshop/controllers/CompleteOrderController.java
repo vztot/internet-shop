114502e0b4f0420760526e89cb48cc9386df48be
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-04-29 14:33:53 +0300

    refactored names

--- src/main/java/mate/academy/internetshop/controllers/CompleteOrderController.java
+++ src/main/java/mate/academy/internetshop/controllers/CompleteOrderController.java
@@ -13,7 +13,7 @@ import mate.academy.internetshop.service.ShoppingCartService;
 import mate.academy.internetshop.service.UserService;
 
 @WebServlet("/orders/new")
-public class AddOrderController extends HttpServlet {
+public class CompleteOrderController extends HttpServlet {
     private static final Injector INJECTOR = Injector.getInstance("mate.academy.internetshop");
     private OrderService orderService = (OrderService) INJECTOR.getInstance(OrderService.class);
     private ShoppingCartService shoppingCartService =
--- src/main/java/mate/academy/internetshop/controllers/GetOrderController.java
+++ src/main/java/mate/academy/internetshop/controllers/GetOrderController.java
@@ -12,7 +12,7 @@ import mate.academy.internetshop.model.Product;
 import mate.academy.internetshop.service.OrderService;
 
 @WebServlet("/orders/view")
-public class ViewOrderController extends HttpServlet {
+public class GetOrderController extends HttpServlet {
     private static final Injector INJECTOR = Injector.getInstance("mate.academy.internetshop");
     private OrderService orderService = (OrderService) INJECTOR.getInstance(OrderService.class);
 
