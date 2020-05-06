c9c9c2d534b7bc3a94f6fa10c1be2db9cb598c0f
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-05-06 03:26:43 +0300

    added solution

---
+++ src/main/java/mate/academy/internetshop/web/filter/AuthorizationFilter.java
@@ -0,0 +1,76 @@
+package mate.academy.internetshop.web.filter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.academy.internetshop.lib.Injector;
+import mate.academy.internetshop.model.Role;
+import mate.academy.internetshop.model.User;
+import mate.academy.internetshop.service.UserService;
+
+public class AuthorizationFilter implements Filter {
+    private static final String USER_ID = "user_id";
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy.internetshop");
+    private UserService userService = (UserService) INJECTOR.getInstance(UserService.class);
+    private Map<String, Set<Role.RoleName>> protectedUrls = new HashMap<>();
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        protectedUrls.put("/users/all", Set.of(Role.RoleName.ADMIN));
+        protectedUrls.put("/products/management", Set.of(Role.RoleName.ADMIN));
+        protectedUrls.put("/products/add", Set.of(Role.RoleName.ADMIN));
+        protectedUrls.put("/products/delete", Set.of(Role.RoleName.ADMIN));
+
+        protectedUrls.put("/orders/new", Set.of(Role.RoleName.USER));
+        protectedUrls.put("/user/orders", Set.of(Role.RoleName.USER));
+        protectedUrls.put("/shoppingCart/products/add", Set.of(Role.RoleName.USER));
+        protectedUrls.put("/shoppingCart/products/delete", Set.of(Role.RoleName.USER));
+        protectedUrls.put("/shoppingCart/products", Set.of(Role.RoleName.USER));
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        String url = req.getServletPath();
+
+        if (protectedUrls.get(url) == null) {
+            chain.doFilter(req, resp);
+            return;
+        }
+
+        Long userId = (Long) req.getSession().getAttribute(USER_ID);
+        User user = userService.get(userId);
+        if (isAuthorized(user, protectedUrls.get(url))) {
+            chain.doFilter(req, resp);
+        } else {
+            req.getRequestDispatcher("/WEB-INF/views/accessDenied.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    private boolean isAuthorized(User user, Set<Role.RoleName> authorizedRoles) {
+        for (Role.RoleName authorizedRole : authorizedRoles) {
+            for (Role userRole : user.getRoles()) {
+                if (authorizedRole.equals(userRole.getRoleName())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/accessDenied.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Access denied!</title>
+</head>
+<body>
+<h1 style="color: red">Access denied!</h1>
+</body>
+</html>
