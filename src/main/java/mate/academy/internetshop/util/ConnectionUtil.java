4726467119daf78350f9fe7b2e0bb23a2f70e6ff
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-05-19 22:30:52 +0300

    edited property

--- src/main/java/mate/academy/internetshop/util/ConnectionUtil.java
+++ src/main/java/mate/academy/internetshop/util/ConnectionUtil.java
@@ -21,7 +21,8 @@ public class ConnectionUtil {
     public static Connection getConnection() {
         Properties dbProperties = new Properties();
         dbProperties.put("user", "root");
-        dbProperties.put("password", "2164092");
+        dbProperties.put("password",
+                "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1MS3lvZVF3QnpBWQ==");
 
         String url = "jdbc:mysql://localhost:3306/" + DB_NAME + "?serverTimezone=" + TIMEZONE;
         try {
